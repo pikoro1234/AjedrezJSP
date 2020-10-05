99334b395d67e502f990b1b690aa1e735a71c395
jorge <donjuan.jfc@gmail.com>
2020-10-05 16:45:26 +0200

    implements js customTablero

---
+++ Ajedrez/web/js/customTablero.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){        
+    
+    $(".filaColumna").click(function(){  
+        
+        
+        //pintado de la ficha aun falta mejorar 
+        $(this).css('background-color','#E0FAEF');   
+                                      
+        var valorFilaColumna = $(this).attr("data"); 
+        
+        $("#valorFilaColumna").attr("value",valorFilaColumna);   
+        
+        var valor = $("#valorFilaColumna").val();       
+        $.ajax({
+            type : 'POST',
+            url: 'Tablero',            
+            data : {valor : valor},
+            success: function(data) {		
+                $('#titulo').html(valor);
+            },
+            error: function() {
+                console.log("No se ha podido obtener la información");
+            }
+        });//fin ajax
+        
+       // $(this).appendTo('.imagenFichas').fadeIn(500);
+         //$(this).appendTo('#another-container').fadeIn(500); 
+          
+    });//fin click ficha
+    
+});//fin document
